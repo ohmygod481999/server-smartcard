7e448bde59e740925d5009c1b145131596dd3731
longvb <vuongbaolong48@gmail.com>
2022-07-12 20:46:13 +0700

    update registration

--- registration.js
+++ registration.js
@@ -52,6 +52,35 @@ router.post("/", async (req, res) => {
     data["traits"]["card_id"] = parseInt(cardId);
     data["traits"]["referer_id"] = parseInt(referrerCode);
 
+    try {
+        // const registRes = await ory.submitSelfServiceRegistrationFlow(flowId, data, "csrf_token_806060ca5bf70dff3caa0e5c860002aade9d470a5a4dce73bcfa7ba10778f481=TS7+yf9wv3Eumvg2i31X7aRh0mwTsEziY2Ck3XOkxj0=");
+        const registRes = await ory.submitSelfServiceRegistrationFlow(
+            flowId,
+            data,
+            cookiesString
+        );
+
+        console.log(
+            "This is the user session: ",
+            registRes.data,
+            registRes.data.identity
+        );
+
+        res.status(registRes.status);
+        res.json({
+            success: true,
+            data: registRes.data,
+        });
+    } catch (error) {
+        console.log(error.response.data);
+        res.status(_.get(error, "response.status") || 500);
+        res.json({
+            success: false,
+            data: error.response.data,
+        });
+        return
+    }
+
     // get parents and push to queue
 
     const referer_id = data["traits"]["referer_id"];
@@ -83,34 +112,6 @@ router.post("/", async (req, res) => {
     // pool.query("SELECT * FROM ", (err, res) => {
     //     console.log(err, res);
     // });
-
-    try {
-        // const registRes = await ory.submitSelfServiceRegistrationFlow(flowId, data, "csrf_token_806060ca5bf70dff3caa0e5c860002aade9d470a5a4dce73bcfa7ba10778f481=TS7+yf9wv3Eumvg2i31X7aRh0mwTsEziY2Ck3XOkxj0=");
-        const registRes = await ory.submitSelfServiceRegistrationFlow(
-            flowId,
-            data,
-            cookiesString
-        );
-
-        console.log(
-            "This is the user session: ",
-            registRes.data,
-            registRes.data.identity
-        );
-
-        res.status(registRes.status);
-        res.json({
-            success: true,
-            data: registRes.data,
-        });
-    } catch (error) {
-        console.log(error.response.data);
-        res.status(_.get(error, "response.status") || 500);
-        res.json({
-            success: false,
-            data: error.response.data,
-        });
-    }
 });
 
 module.exports = router;
