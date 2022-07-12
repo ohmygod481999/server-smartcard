330ccbd10cef8125150f3fcb59147e59da29524b
longvb <vuongbaolong48@gmail.com>
2022-07-12 20:03:57 +0700

    update db connection

--- pg-client.js
+++ pg-client.js
@@ -1,6 +1,14 @@
 const { Pool, Client } = require('pg')
-const { DB_CONNECTION_URL } = require('./config')
+const { DB_CONNECTION_URL, DB_HOSTNAME, DB_USERNAME, DB_DATABASE, DB_PASSWORD, DB_PORT, CA_CERT } = require('./config')
 
 exports.pool = new Pool({
-  connectionString: DB_CONNECTION_URL,
+  // connectionString: DB_CONNECTION_URL,
+  user: DB_USERNAME,
+  host: DB_HOSTNAME,
+  database: DB_DATABASE,
+  password: DB_PASSWORD,
+  port: DB_PORT,
+  ssl: {
+    ca: CA_CERT
+  }
 })
