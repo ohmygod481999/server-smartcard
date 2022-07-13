56e7d5ec03abc0d3b7b18aca4b2f8f81f95a2fd4
longvb <vuongbaolong48@gmail.com>
2022-07-13 17:12:42 +0700

    update registration

---
+++ queries/index.js
@@ -0,0 +1,35 @@
+const { gql } = require("graphql-request");
+
+exports.INSERT_ACCOUNT_MUTATION = gql`
+    mutation insertAccount($ory_id: uuid!, $referer_id: Int) {
+        insert_account(objects: { ory_id: $ory_id, referer_id: $referer_id }) {
+            affected_rows
+            returning {
+                id
+            }
+        }
+    }
+`;
+
+exports.CONNECT_ACCONT_TO_CARD_MUTATION = gql`
+    mutation updateCard($card_id: Int!, $account_id: Int!) {
+        update_card(
+            where: { id: { _eq: $card_id } }
+            _set: { account_id: $account_id }
+        ) {
+            affected_rows
+        }
+    }
+`;
+
+exports.CREATE_WALLET_MUTATION = gql`
+    mutation createWallet($account_id: Int!, $wallet_type: Int!) {
+        insert_wallet(
+            objects: { account_id: $account_id, type: $wallet_type }
+        ) {
+            returning {
+                id
+            }
+        }
+    }
+`;
--- registration.js
+++ registration.js
@@ -3,6 +3,12 @@ const { ory } = require("./ory");
 const _ = require("lodash");
 const amqp = require("amqplib/callback_api");
 const { getParents } = require("./db-queries");
+const { graphQLClient } = require("./graphql-client");
+const {
+    INSERT_ACCOUNT_MUTATION,
+    CONNECT_ACCONT_TO_CARD_MUTATION,
+    CREATE_WALLET_MUTATION,
+} = require("./queries");
 // const { pool } = require("./pg-client");
 
 let channel;
@@ -42,15 +48,15 @@ router.post("/", async (req, res) => {
 
     const { flowId, data, myData } = req.body;
 
-    const { cardId, referrerCode } = myData;
+    let { cardId, referrerCode } = myData;
 
     const cookies = Object.keys(req.cookies).map(
         (key) => `${key}=${req.cookies[key]}`
     );
     const cookiesString = cookies.join(";");
 
-    data["traits"]["card_id"] = parseInt(cardId);
-    data["traits"]["referer_id"] = parseInt(referrerCode);
+    // data["traits"]["card_id"] = parseInt(cardId);
+    // data["traits"]["referer_id"] = parseInt(referrerCode);
 
     try {
         // const registRes = await ory.submitSelfServiceRegistrationFlow(flowId, data, "csrf_token_806060ca5bf70dff3caa0e5c860002aade9d470a5a4dce73bcfa7ba10778f481=TS7+yf9wv3Eumvg2i31X7aRh0mwTsEziY2Ck3XOkxj0=");
@@ -71,6 +77,83 @@ router.post("/", async (req, res) => {
             success: true,
             data: registRes.data,
         });
+
+        const oryId = registRes.data.identity.id;
+        cardId = parseInt(cardId);
+        const referer_id = parseInt(referrerCode);
+
+        // insert account
+
+        const insertAccountRes = await graphQLClient.request(
+            INSERT_ACCOUNT_MUTATION,
+            {
+                ory_id: oryId,
+                referer_id: referer_id ? parseInt(referer_id) : null,
+            }
+        );
+
+        // connect card with account
+
+        const account_id = insertAccountRes.insert_account.returning[0].id;
+
+        const updateRes = await graphQLClient.request(
+            CONNECT_ACCONT_TO_CARD_MUTATION,
+            {
+                card_id: cardId,
+                account_id: account_id,
+            }
+        );
+
+        // create wallet
+        const createWalletRes = await Promise.all([
+            graphQLClient.request(CREATE_WALLET_MUTATION, {
+                account_id: account_id,
+                wallet_type: 0,
+            }),
+            graphQLClient.request(CREATE_WALLET_MUTATION, {
+                account_id: account_id,
+                wallet_type: 1,
+            }),
+        ]);
+
+        // thuong nguoi dung moi
+        channel.sendToQueue(
+            "transaction",
+            Buffer.from(
+                JSON.stringify({
+                    account_id: account_id,
+                    transaction_type: 1, // 1: reward for new user
+                    payload: {},
+                    date: new Date(),
+                })
+            )
+        );
+
+        // get parents and push to queue
+
+        const parents = await getParents(referer_id);
+
+        parents.forEach((parent, i) => {
+            const { id, name, referer_id, is_agency } = parent;
+
+            if (i === 0 || is_agency) {
+                // neu la nguoi gioi thieu truc tiep hoac nguoi do la agency thi thuong?
+                channel.sendToQueue(
+                    "transaction",
+                    Buffer.from(
+                        JSON.stringify({
+                            account_id: id,
+                            transaction_type: 0, // 0: refer
+                            payload: {
+                                level: i,
+                                is_agency,
+                            },
+                            date: new Date(),
+                        })
+                    )
+                );
+            }
+        });
     } catch (error) {
         console.log(error.response.data);
         res.status(_.get(error, "response.status") || 500);
@@ -78,36 +161,8 @@ router.post("/", async (req, res) => {
             success: false,
             data: error.response.data,
         });
-        return
+        return;
     }
-
-    // get parents and push to queue
-
-    const referer_id = data["traits"]["referer_id"];
-
-    const parents = await getParents(referer_id);
-
-    parents.forEach((parent, i) => {
-        const { id, name, referer_id, is_agency } = parent;
-
-        if (i === 0 || is_agency) {
-            // neu la nguoi gioi thieu truc tiep hoac nguoi do la agency thi thuong?
-            channel.sendToQueue(
-                "transaction",
-                Buffer.from(
-                    JSON.stringify({
-                        account_id: id,
-                        transaction_type: 0, // 0: refer
-                        payload: {
-                            level: i,
-                            is_agency,
-                        },
-                        date: new Date(),
-                    })
-                )
-            );
-        }
-    });
 
     // pool.query("SELECT * FROM ", (err, res) => {
     //     console.log(err, res);
